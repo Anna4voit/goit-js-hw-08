a05efa717984fac1210a3738efae7477ce53aacb
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2023-10-26 22:33:49 +0300

    upgrade-4
    fix bugs

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -14,11 +14,11 @@ const LS_TIME_KEY = 'videoplayer-current-time';
 //функція для збереження  поточного часу відтворення відео
 const getCurrentTime = function (currentTime) {
   const seconds = currentTime.seconds;
-  localStorage.setItem(LS_TIME_KEY, JSON.stringify(seconds));
+  localStorage.setItem(LS_TIME_KEY, seconds);
 };
 
 // подія timeupdate - оновлення часу відтворення.
 player.on('timeupdate', throttle(getCurrentTime, 1000));
 
 //Під час перезавантаження сторінки відновлюємо відтворення зі збереженої позиції.
-player.setCurrentTime(JSON.parse(localStorage.getItem(LS_TIME_KEY)) || 0);
+player.setCurrentTime(localStorage.getItem(LS_TIME_KEY) || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,8 +9,28 @@ const feedbackForm = document.querySelector('.feedback-form');
 const LS_KEY = 'feedback-form-state';
 //змінна для збереження значеня полів форми
 let feedbackFormData = {};
+
+//функція для перезавантаження сторінки, перевіряємо чи існує відповідний ключ в сховищі
+//тоді отримуємо в змінну значення із сховища і
+//значенням полів форми присваюємо  відповідні значення із сховища
+const reload = () => {
+  try {
+    const data = localStorage.getItem(LS_KEY);
+    if (!data) {
+      return;
+    }
+    feedbackFormData = JSON.parse(data);
+    Object.entries(feedbackFormData).forEach(([key, val]) => {
+      feedbackForm.elements[key].value = val;
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
 //функція для перезавантаження сторінки
 reload();
+
 //на формі просдуховуємо дві події інпут і сабміт
 feedbackForm.addEventListener('input', throttle(onInput, 500));
 feedbackForm.addEventListener('submit', onSubmit);
@@ -18,7 +38,7 @@ feedbackForm.addEventListener('submit', onSubmit);
 //функція для події інпут
 function onInput(event) {
   //в змінну записуємо поточні значеня полів форми
-  feedbackFormData[event.target.name] = event.target.value;
+  feedbackFormData[event.target.name] = event.target.value.trim();
   localStorage.setItem(LS_KEY, JSON.stringify(feedbackFormData));
   //або через елементи форми
   //const { email, message } = feedbackForm.elements;
@@ -28,27 +48,13 @@ function onInput(event) {
   // };
 }
 
-//функція для перезавантаження сторінки, перевіряємо чи існує відповідний ключ в сховищі
-//тоді отримуємо в змінну значення із сховища і
-//значенням полів форми присваюємо  відповідні значення із сховища
-function reload() {
-  if (localStorage.getItem(LS_KEY)) {
-    feedbackFormData = JSON.parse(localStorage.getItem(LS_KEY));
-    const { email, message } = feedbackForm.elements;
-    email.value = feedbackFormData.email || '';
-    message.value = feedbackFormData.message || '';
-  }
-}
 //функція при сабміті, виводить в консоль об'єкт значень полів форми
 //очищує сховище і форму
 function onSubmit(event) {
   event.preventDefault();
   const form = event.target;
-  //перевірка, якщо поля не заповнені
-  if (form.elements.email.value === '' || form.elements.message.value === '') {
-    return alert('Please fill in all the fields!');
-  }
   console.log(feedbackFormData);
   form.reset();
   localStorage.removeItem(LS_KEY);
+  feedbackFormData = {};
 }
